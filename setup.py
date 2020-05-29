bb3464aa5f6af6e53d530751110d869bbc20f4e3
David Domínguez <dominguezbarradavid33@gmail.com>
2020-05-29 13:39:01 +0200

    Add setup requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 import glob
 
+with open('requirements.txt') as f:
+    install_requires = f.read().split()
+
 setup(
     name="drling",
     version="0.0.1",
-    scripts=glob.glob("scripts/drling_*.py")
+    scripts=glob.glob("scripts/drling_*.py"),
+    install_requires=install_requires
 )
