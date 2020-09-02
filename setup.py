69b9718c135008629995d16bc6dc10283e90acee
David Domínguez <dominguezbarradavid33@gmail.com>
2020-09-02 22:22:33 +0200

    Add __init__ and update install_requirements in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import setup
 import glob
 
 with open('requirements.txt') as f:
-    install_requires = f.read().split()
+    install_requires = [
+        "gym==0.17.1",
+        "matplotlib==3.2.1",
+        "numpy==1.18.2",
+        "pandas==1.0.3",
+        "tensorflow>=2.0.2",
+        "tqdm==4.45.0",
+        "PyYAML==5.3.1",
+    ]
 
 setup(
     name="drling",
-    version="0.0.1",
+    version="0.0.3",
     scripts=glob.glob("scripts/drling_*.py"),
     install_requires=install_requires
 )
